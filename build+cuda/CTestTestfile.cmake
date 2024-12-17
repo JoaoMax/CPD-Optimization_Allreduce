3f97f71c2273f6e2510c2144820999970fb116ea
JoaoMax <joaochavezsalas@gmail.com>
2024-12-17 09:28:36 -0500

    Add files via upload

---
+++ build+cuda/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/princce/ext_mpi_collectives
+# Build directory: /home/princce/ext_mpi_collectives/build+cuda
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("src")
+subdirs("tests")
